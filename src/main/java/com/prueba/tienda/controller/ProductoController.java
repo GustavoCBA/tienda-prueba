cf79e181fa56e22b962bd3d1192bb65f49f3f91d
gustavo adolfo montoya perez <gustavomontoya618@hotmail.com>
2022-08-11 16:43:53 -0500

    guardar producto y crear

--- src/main/java/com/prueba/tienda/controller/ProductoController.java
+++ src/main/java/com/prueba/tienda/controller/ProductoController.java
@@ -1,5 +1,43 @@
+
 package com.prueba.tienda.controller;
 
+import org.slf4j.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.prueba.tienda.model.Producto;
+import com.prueba.tienda.model.Usuario;
+import com.prueba.tienda.service.ProductoService;
+
+@Controller
+@RequestMapping("/productos")
 public class ProductoController {
+	private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class);
+	@Autowired
+	private ProductoService productoservice;
+	
+	@GetMapping("")
+	public String show() {
+		return "productos/show";
+		
+	}
+	@GetMapping("/create")
+	public String create() {
+		return  "productos/create";
+		
+	}
+	
+	@PostMapping("/save")
+	public String save(Producto producto) {
+		LOGGER.info("Objeto producto{}",producto);
+		Usuario u = new Usuario(1,"","","","","","","");
+		producto.setUsuario(u);
+		productoservice.save(producto);
+		return "redirect:/productos";
+		
+	}
 
 }
