39eaa8d2739e48a6b85b02f5ba9be366fe5e156b
gustavo adolfo montoya perez <gustavomontoya618@hotmail.com>
2022-08-10 20:28:43 -0500

    Creacion estructura proyecto y calses model

---
+++ src/main/java/com/prueba/tienda/TiendaPruebaApplication.java
@@ -0,0 +1,14 @@
+package com.prueba.tienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class TiendaPruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaPruebaApplication.class, args);
+	}
+
+}
